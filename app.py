76e7e2a4c21786e837982b41d6899a6f2aa6b8d2
marquesafonso <afonso.cunha.marques@gmail.com>
2024-08-23 16:32:10 +0100

    add delete_cache daily. add copy button to textbox

--- app.py
+++ app.py
@@ -2,19 +2,19 @@ import gradio as gr
 from src.transcriber import transcriber
 
 def main():
-    with gr.Blocks(title='multilang-asr-transcriber') as demo:
+    with gr.Blocks(title='multilang-asr-transcriber', delete_cache=(86400, 86400)) as demo:
         gr.Markdown('# multilang-asr-transcriber')
         gr.Markdown('### A multilingual automatic speech transcription tool using [faster-whisper](https://github.com/SYSTRAN/faster-whisper). Supports translation to english and user setting of max words per line.')
         video_file = gr.File(file_types=["video"],type="filepath", label="Upload a video")
         max_words_per_line = gr.Number(value=6, label="Max words per line")
         task = gr.Dropdown(choices=["transcribe", "translate"], value="transcribe", label="Select Task")
-        text_output = gr.Textbox(label="Text transcription")
+        text_output = gr.Textbox(label="Text transcription", show_copy_button=True)
         srt_file = gr.File(file_count="single", type="filepath", file_types=[".srt"], label="SRT file")
         gr.Interface(transcriber,
                     inputs=[video_file, max_words_per_line, task],
-                    outputs=[text_output,srt_file],
+                    outputs=[text_output, srt_file],
                     allow_flagging="never")
-    demo.launch(allowed_paths=["https://marquesafonso-multilang-asr-transcriber.hf.space/file=/tmp/gradio/", "file=/tmp/gradio/", "/tmp/gradio/"])
+    demo.launch()
 
 if __name__ == '__main__':
     main()
