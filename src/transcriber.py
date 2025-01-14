1ae95112ef944a9480e7fcf896d5dc93b7730179
Afonso Marques <marquesafonso@users.noreply.huggingface.co>
2025-01-14 15:03:06 +0000

    fix transcriber to deal with audio or video

--- src/transcriber.py
+++ src/transcriber.py
@@ -43,12 +43,16 @@ def write_srt(segments, max_words_per_line, srt_path):
         file.write(result)
         return result, srt_path, " ".join(result_clean)
 
-def transcriber(video_input:gr.File,
+def transcriber(file_input:gr.Audio | gr.Video,
                 max_words_per_line:int,
                 task:str,
                 model_version:str):
+    
     srt_filepath = os.path.normpath(f"{video_input.split('.')[0]}.srt")
-    audio_input = convert_video_to_audio(video_input)
+    if type(file_input) == gr.Video:
+        audio_input = convert_video_to_audio(file_input)
+    else:
+        audio_input = file_input
     model = WhisperModel(model_version, device="cpu", compute_type="int8")
     segments, _ = model.transcribe(
         audio_input,
