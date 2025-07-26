ae52f76c5daf9b9abbae45c9e37a1ab0084b33bc
marquesafonso <afonso.cunha.marques@gmail.com>
2025-07-26 16:39:02 +0200

    retry with Textbox

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def main():
                 text_output = gr.Textbox(label="SRT Text transcription")
                 srt_file = gr.File(file_count="single", type="filepath", file_types=[".srt"], label="SRT file")
                 text_clean_output = gr.Textbox(label="Text transcription")
-                json_output = gr.JSON(label="JSON Transcription")
+                json_output = gr.Textbox(label="JSON Transcription")
                 gr.Interface(
                     fn=transcriber,
                     inputs=[file, file_type, max_words_per_line, task, model_version],
@@ -38,7 +38,7 @@ def main():
                 text_output = gr.Textbox(label="SRT Text transcription")
                 srt_file = gr.File(file_count="single", type="filepath", file_types=[".srt"], label="SRT file")
                 text_clean_output = gr.Textbox(label="Text transcription")
-                json_output = gr.JSON(label="JSON Transcription")
+                json_output = gr.Textbox(label="JSON Transcription")
                 gr.Interface(
                     fn=transcriber,
                     inputs=[file, file_type, max_words_per_line, task, model_version],
